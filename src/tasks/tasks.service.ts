44866b9083e3fc5870aee8c928f218a2d73a0954
Rasheedah Zubayr <olamiderasheedat@gmail.com>
2023-03-10 22:12:04 +0100

    Deleting a task from the database

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -78,6 +78,14 @@ export class TasksService {
     //     return task;
     // }
 
+    async deleteTask(id: string): Promise<void> {
+        const result = await this.tasksRepository.delete(id)
+        
+        if (result.affected === 0){
+            throw new NotFoundException(`Task with ID "${id}" not found`)
+        }
+    }
+
     // deleteTask(id: string): void {
     //     const found = this.getTaskById(id);
     //     this.tasks = this.tasks.filter((task) => task.id !== found.id);
