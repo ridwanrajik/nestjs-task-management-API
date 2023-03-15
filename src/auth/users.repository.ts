18bf64a89fe70fb2bb1dab640b0dacf25f2724ef
Rasheedah Zubayr <olamiderasheedat@gmail.com>
2023-03-15 17:23:40 +0100

    Handling multiple username

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { ConflictException, Injectable, InternalServerErrorException } from "@nestjs/common";
 import { DataSource, Repository } from "typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { User } from "./user.entity";
@@ -17,6 +17,15 @@ export class UsersRepository extends Repository<User> {
     async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
         const user = this.create({ username, password });
-        await this.save(user);
+        try {
+            await this.save(user);
+        } catch (error) {
+            if (error.code === '23505') {
+                // duplicate username
+                throw new ConflictException('Username already exists');
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
     }
 }
