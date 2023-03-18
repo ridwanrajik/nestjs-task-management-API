af5fe3c968ca41c9bbd2a97bff49bcd516f063ac
Rasheedah Zubayr <olamiderasheedat@gmail.com>
2023-03-18 14:47:10 +0100

    Implementing JWT validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Body, Controller, Post, Req} from '@nestjs/common';
+import { AuthService } from './auth.service'; 
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+// import { UseGuards } from '@nestjs/common';
+// import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -15,4 +17,10 @@ export class AuthController {
     signIn(@Body() authCredentialsDto: AuthCredentialsDto): Promise<{ accessToken: string }> {
         return this.authService.signIn(authCredentialsDto);
     }
+
+    // @Post('/test')
+    // @UseGuards(AuthGuard())
+    // test(@Req() req) {
+    //     console.log(req);
+    // }
 }
