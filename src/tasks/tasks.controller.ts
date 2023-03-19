cbec881b06b0e0d3162d66d82aacbf3759825939
Rasheedah Zubayr <olamiderasheedat@gmail.com>
2023-03-19 23:04:42 +0100

    Updating status of task belonging to a particular user

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -58,11 +58,11 @@ export class TasksController {
     //     return this.tasksService.deleteTask(id);
     // }
 
-    // @Patch('/:id/status')
-    // updateTaskStatus( @Param('id') id: string, @Body() updateTaskStatusDto: UpdateTaskStatusDto): Promise<Task> {
-    //     const {status} = updateTaskStatusDto
-    //     return this.tasksService.updateTaskStatus(id, status);
-    // }
+    @Patch('/:id/status')
+    updateTaskStatus( @Param('id') id: string, @Body() updateTaskStatusDto: UpdateTaskStatusDto, @GetUser() user: User): Promise<Task> {
+        const {status} = updateTaskStatusDto
+        return this.tasksService.updateTaskStatus(id, status, user);
+    }
 
     // @Patch('/:id/status')
     // updateTaskStatus( @Param('id') id: string, @Body() updateTaskStatusDto: UpdateTaskStatusDto): Task {
